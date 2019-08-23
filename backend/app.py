46eb89266e7e8e9ac1e6c03d4c26ad701ba1d5b4
cn-wx <xuwang2@student.unimelb.edu.au>
2019-08-24 04:33:04 +1000

    [ADD] front end

--- backend/app.py
+++ backend/app.py
@@ -9,12 +9,12 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-models = joblib.load('./models/models.pkl')
-for i in range(len(models)):                
-    y_val_prdt[:,i] = model[i].predict_proba(test[predictors])[:,1]
-    test_preds = y_val_prdt.mean(axis=1)
-    y_val_pred = (test_preds >0.63).astype(np.int32)
-    # print(y_val_prdt, test_preds, y_val_pred)
+# models = joblib.load('./models/models.pkl')
+# for i in range(len(models)):                
+#     y_val_prdt[:,i] = model[i].predict_proba(test[predictors])[:,1]
+#     test_preds = y_val_prdt.mean(axis=1)
+#     y_val_pred = (test_preds >0.63).astype(np.int32)
+#     # print(y_val_prdt, test_preds, y_val_pred)
 
 if __name__ == '__main__':
     app.run(debug=True)
