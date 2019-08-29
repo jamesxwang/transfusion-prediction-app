56a6b9a97f64de31e26f332c71cf8221cb0fa0a3
cn-wx <xuwang2@student.unimelb.edu.au>
2019-08-29 20:27:21 +1000

    [UPDATE] backend

---
+++ frontend/vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    productionSourceMap: false,
+    publicPath: process.env.NODE_ENV === 'production' ? 'http://106.52.236.232:5000/' : '/',
+    devServer: { 
+      disableHostCheck: true,
+      host: '0.0.0.0',
+      port: 8080,
+      proxy: {
+        '/api': {
+          target: 'http://127.0.0.1:5000',
+          ws: true,
+          changeOrigin: true
+        }
+      }
+    },
+  }
